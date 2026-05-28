8d0cc98c42091a38ece2b843f31fa09294642e1a
MarcioBelucci <jubeluccips4@gmail.com>
2026-05-28 15:42:42 -0300

    Add replay option and warning for 10 guesses in word guessing game - part 2

--- W04WordPuzzle.py
+++ W04WordPuzzle.py
@@ -1,34 +1,34 @@
-secreat = "mosiah"
-letter_secreat = list(secreat)
-print("Welcome to the word guessing game!")
-guess = ""
-hint = "_" * len(secreat)
-count = 0
-while guess != secreat:
-    count += 1
+# I added two new features. The first is to give the players the option to play again when they guess the secret word. If they choose "no," the game will display a message thanking them for playing.
+# The second is a message to advise the player if they took 10 guesses.
+play = "yes"
+while play == "yes":
+    secreat = "mosiah"
+    letter_secreat = list(secreat)
+    guess = ""
+    hint = "_ " * len(secreat)
+    count = 0 
+    print("Welcome to the word guessing game!")
     print(f"\nYour hint is: {hint}")
-    guess = input("What is your guess? ").lower()
-
-    for i in range(len(guess)):
-        letter = guess[i]
-        letter_secreat = secreat[i]
-        if letter == letter_secreat:
-            print(letter.upper(), end="")
-        #elif letter != letter_secreat:
-        #    print(letter.lower(), end="")
-        else:
-            print("_", end="")            
-    
-print("\nCongratulations! You guessed it!")
-print(f"It took you {count} guesses.")
-
-#for i in range(len(word)):
-#    letter = word[i]
-#    if letter == favorite_letter:
-#        print("_", end="")
-#    else:
-#        print(letter.lower(), end="")
-
-
-# for s in range(len(secreat)):
-        #letter_secreat = secreat[s]
+    while guess != secreat:
+        count += 1
+        guess = input("What is your guess? ").lower()    
+        if len(guess) == len(secreat):
+            hint = ""
+            for i in range(len(guess)):
+                letter = guess[i]
+                letter_secreat = secreat[i]
+                if letter == letter_secreat:
+                    hint += letter.upper() + " "
+                elif letter in secreat:
+                    hint += letter.lower() + " "
+                else:
+                    hint += "_ "
+            print(f"Your hint is: {hint}") 
+        else:        
+            print("Sorry, the guess must have the same number of letters as the secret word.\n")   
+        if count == 10 and guess != secreat:
+            print("\nPlease, pay attention! You already took 10 guesses.\n")
+    print("\nCongratulations! You guessed it!")
+    print(f"It took you {count} guesses.")
+    play = input("Would you like to play again (yes/no)? ").lower()
+print("Thank you for playing. See you next time.")
